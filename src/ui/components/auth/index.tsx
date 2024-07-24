d360d368cea6925c8586fdd8ffef279d744a87c2
lucas vinicius <lucassilved@gmail.com>
2024-07-24 19:48:23 -0300

    feat: add point of distributing all auth components

---
+++ src/ui/components/auth/index.tsx
@@ -0,0 +1,19 @@
+import { Container } from './container';
+import { VerticalBanner } from './vertical-banner';
+import { FormContainer } from './form-container';
+import { FormTitle } from './form-title';
+import { FormContent } from './form-content';
+import { Input } from './input';
+import { Button } from './button';
+import { AuthBase } from './auth-base';
+
+export const Auth = Object.freeze({
+  Container,
+  VerticalBanner,
+  FormContainer,
+  FormTitle,
+  FormContent,
+  Input,
+  Button,
+  AuthBase,
+});
