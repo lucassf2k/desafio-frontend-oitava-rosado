2a25a8d147797cd842aaaa70411fed9a53e7100a
lucas vinicius <lucassilved@gmail.com>
2024-07-24 19:45:57 -0300

    feat: add auth form input component

---
+++ src/ui/components/auth/input/index.tsx
@@ -0,0 +1,16 @@
+import S from './styles.module.css';
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  htmlFor: string;
+}
+
+export function Input(props: InputProps) {
+  return (
+    <div className={S.authFormField}>
+      <label htmlFor={props.htmlFor}>{props.label}</label>
+      <input {...props} />
+    </div>
+  );
+}
