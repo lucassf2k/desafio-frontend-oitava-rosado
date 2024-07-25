c1c7dd0fad339dd02a401e494dee0ca4f307799f
lucas vinicius <lucassilved@gmail.com>
2024-07-25 17:20:33 -0300

    feat: add password input with functionality to show password

---
+++ src/ui/components/auth/input/password.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import eyeIcon from '../../../assets/icons/eye.svg';
+import S from './styles.module.css';
+
+export interface InputPasswordProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  htmlFor: string;
+}
+
+export function InputPassword(props: InputPasswordProps) {
+  const [passwordType, setPasswordType] = useState('password');
+
+  const handleTogglePasswordVisibility = () => {
+    if (passwordType === 'password') setPasswordType('text');
+    if (passwordType === 'text') setPasswordType('password');
+  };
+
+  return (
+    <div className={S.authFormField}>
+      <label htmlFor={props.htmlFor}>{props.label}</label>
+      <input {...props} type={passwordType} />
+      <button type="button" onClick={handleTogglePasswordVisibility}>
+        <img src={eyeIcon} alt="Ícone de um olho" />
+      </button>
+    </div>
+  );
+}
